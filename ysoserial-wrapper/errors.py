7b878f7fb709e0316f66236ef8b3d7a9f2e4bb0c
Elie Saad <eliesaad7@gmail.com>
2019-04-13 11:39:30 +0300

    fix: exceptions and silent fails

--- ysoserial-wrapper/errors.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["JarNotFoundException", "EnvironmentKeyNotFoundException"]
-
-
-class JarNotFoundException(Exception):
-    """Raised when a jar is not accessible"""
-
-
-class EnvironmentKeyNotFoundException(Exception):
-    """Raised when the KEY environment variable is missing"""
